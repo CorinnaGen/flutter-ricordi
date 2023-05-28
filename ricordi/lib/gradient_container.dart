48b381c7d39e89e34723cd8e51f108e770fe5ce8
Corinna <corinnagenchi@gmail.com>
2023-05-28 17:12:16 +0200

    making styled text and container widgets customizable

--- ricordi/lib/gradient_container.dart
+++ ricordi/lib/gradient_container.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:ricordi/text_container.dart';
+import 'package:ricordi/styled_text.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.colors, {super.key});
+
+  final List<Color> colors;
+
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(115, 209, 64, 202),
-          Color.fromARGB(120, 87, 96, 195)
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: colors, begin: startAlignment, end: endAlignment),
       ),
       child: const Center(
-        child: TextContainer(),
+        child: StyledText('Ricordi'),
       ),
     );
   }
--- ricordi/lib/main.dart
+++ ricordi/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:ricordi/gradient_container.dart';
 
+final List<Color> firstScreenColors = [
+  Color.fromARGB(115, 209, 64, 202),
+  Color.fromARGB(120, 87, 96, 195)
+];
+
 void main() {
   runApp(
     const MaterialApp(
-      home: Scaffold(body: GradientContainer()),
+      home: Scaffold(body: GradientContainer(firstScreenColors)),
     ),
   );
 }
